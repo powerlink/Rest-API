311462d43f91825c7eab9c947b637a00afff18de
Powerlink CRM <amir@powerlink.co.il>
2017-03-27 15:59:55 +0300

    Update query-python.py

--- Query/query-python.py
+++ Query/query-python.py
@@ -1,1 +1,19 @@
- 
+ import requests
+import json
+
+url = 'https://secure.powerlink.co.il/api/query'
+token_id = '73994acf-cd16-48bd-b8e1-17bc8f'
+
+data = {
+   “page_number": 1,
+    "objecttype" : 1,
+    "page_size" : 100,
+    "query" :“(id = 123)"    #optional field
+    “fields”: ”id”,    #optional field
+    "sort_by": “createdon”,    #optional field
+    “sort_type”: ”desc”, #optional field
+}
+
+headers = {'Content-type': 'application/json', 'tokenId': token_id, 'utc_time' : str(1)}
+response = requests.post(url + "/api/query", data=json.dumps(data), headers=headers)
+return json.loads(response.content)['data']
