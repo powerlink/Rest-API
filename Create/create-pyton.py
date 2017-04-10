009bd80654740feff4c3e4bd3dc3458d1d3ad6b3
Powerlink CRM <amir@powerlink.co.il>
2017-04-10 10:58:16 +0300

    Update create-pyton.py

--- Create/create-pyton.py
+++ Create/create-pyton.py
@@ -10,6 +10,6 @@ data = {
 
 url = 'https://secure.powerlink.co.il/api/record/account'
 token_id = '73994acf-cd16-48bd-b8e1-17bc8f'
-headers = {'Content-type': 'application/json', 'tokenId': token_id, 'utc_time' : str(1)}
+headers = {'Content-type': 'application/json', 'tokenId': token_id}
 response = requests.post(url, data=str(data), headers=headers)
 return json.loads(response.content)['data']['Record']
