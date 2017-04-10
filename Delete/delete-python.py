9108cd1a2c7b2b7eef2375899893e3d9a5638927
Powerlink CRM <amir@powerlink.co.il>
2017-04-10 10:58:33 +0300

    Update delete-python.py

--- Delete/delete-python.py
+++ Delete/delete-python.py
@@ -5,6 +5,6 @@ url = 'https://secure.powerlink.co.il/api/record/account/'
 token_id = '73994acf-cd16-48bd-b8e1-17bc8f'
 object_id = '12345f-cd16-48bd-b8'
 
-headers = {'Content-type': 'application/json', 'tokenId': token_id, 'utc_time' : str(1)}
+headers = {'Content-type': 'application/json', 'tokenId': token_id}
 response = requests.delete(url + str(object_id), headers=headers)
 return json.loads(response.content)['data']
