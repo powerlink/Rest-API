4e6ba0a414cfc10192b288c373d3710dafe7ef67
Powerlink CRM <amir@powerlink.co.il>
2019-07-14 00:03:56 +0300

    Update delete-c.cs

--- Delete/delete-c.cs
+++ Delete/delete-c.cs
@@ -5,8 +5,8 @@ using System.IO;
 
  using (WebClient client = new WebClient())
             {
-                string tokenid = "0588209E-2715-419F-7777-732DABBDFE61"; //זה של dev אז להחליף לפני שמעלים
-                string objectid = "332db2bf-3694-4f80-7777-99f87b5aab56";
+                string tokenid = "xxxxx-xxxx-xxxxx-xxxxx"; //זה של dev אז להחליף לפני שמעלים
+                string objectid = "xxxxx-xxxx-xxxxx-xxxxx";
                 client.Encoding = System.Text.Encoding.UTF8;
                 client.Headers.Set("tokenId", tokenid);
                 string result = client.UploadString("https://secure.powerlink.co.il/api/record/account/" + objectid, "DELETE", "");
