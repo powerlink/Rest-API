a8ed18852666311a4bc6658d0801f818425ce76f
Powerlink CRM <amir@powerlink.co.il>
2019-07-14 00:19:09 +0300

    Update delete-php.php

--- Delete/delete-php.php
+++ Delete/delete-php.php
@@ -1,11 +1,11 @@
-$objectid = '332DB2BF-3694-4F80-B82F-99F812345';		
+$objectid = 'xxxxxx-xxxxx-xxxxx-xxxxx';		
 $url =https://secure.powerlink.co.il/api/record/account/'.$objectid;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,$url);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
     'Content-Type: application/json',
-    'tokenid: 0588209E-2715-419F-A913-732DABB12345',                                                                                
+    'tokenid: xxxxxx-xxxxx-xxxxx-xxxxx',                                                                                
     'Content-Length: ' . strlen($data_string))                                                                       
 ); 
     $result = curl_exec($ch);
