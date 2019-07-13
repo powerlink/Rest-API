82a6f84662a8e6b212260d7a007a5536905c3b62
Powerlink CRM <amir@powerlink.co.il>
2019-07-14 00:18:07 +0300

    Update update-python.py

--- Update/update-python.py
+++ Update/update-python.py
@@ -1,9 +1,9 @@
 import requests
 import json
 
-object_id = '12345cf-cd16-48bd-b8e1'
+object_id = 'e0f67202cae6cca49645cd076637e9e1f41b0eeawerwerwerwr'
 url = 'https://secure.powerlink.co.il/api/record/account/'
-token_id = '73994acf-cd16-48bd-b8e1-17bc8f'
+token_id = 'e0f67202cae6cca49645cd076637e9e1f41b0eeawerwerwereww'
 headers = {'Content-type': 'application/json', 'tokenId': token_id}
 response = requests.get(url + str(object_id), data=str(data), headers=headers)
 return json.loads(response.content)['data']
